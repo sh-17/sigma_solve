9819b3d2d8dc329c5e322b905c9df74e83b7f93b
sh-17 <133085428+sh-17@users.noreply.github.com>
2023-10-04 11:23:02 +0530

    Test
    INTRO_TASK

---
+++ CORE_PYTHON/Hetvi_Introduction_Test_1.py
@@ -0,0 +1,23 @@
+# left star
+User = int(input("Enter Number:"))
+for i in range(1,User+1):
+    print("*"*i)
+
+# output :
+"""
+*
+**
+***
+****
+*****
+
+"""
+
+# reverse string
+
+n = "Hey! I'm An Artist"
+rev = n[::-1]
+print()
+print('Reverse String:', rev)
+
+""" Output : Reverse String : tsirtrA nA m'I !yeH """
