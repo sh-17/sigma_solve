24516bad15b7a379bd2773c311a9e1739f4f4573
sh-17 <133085428+sh-17@users.noreply.github.com>
2023-10-31 00:45:09 +0530

    Update hetvi_oops_test3.py

--- Oop' s Concept/hetvi_oops_test3.py
+++ Oop' s Concept/hetvi_oops_test3.py
@@ -1,99 +1,100 @@
-class User:
-    def __init__(self, username, password):
-        self.username = username
-        self.__password = password  # Password is private
 
-    def change_password(self, new_password):
-        self.__password = new_password
-        print("Password changed successfully.")
+from abc import ABC, abstractmethod
+class Book:
+    def __init__(self, title, author, isbn, price):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.price = price
+        self.is_available = True
 
-    def login(self, password):
-        return self.__password == password
+    def __str__(self):
+        return f"Title: {self.title}\nAuthor: {self.author}\nISBN: {self.isbn}\nPrice: Rs.{self.price}\nAvailable: {self.is_available}"
 
+class Library(ABC): #INHERITS FROM ABC CLASS
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+        self.users = {}  # Dictionary to store registered users
 
-class Student(User):
-    def __init__(self, username, password, student_id):
-        super().__init__(username, password)
-        self.student_id = student_id
+    @abstractmethod
+    def add_book(self, book):  # Define an abstract method
+        pass
 
+    def remove_book(self, isbn):
+        for book in self.books:
+            if book.isbn == isbn:
+                self.books.remove(book)
+                return f"{book.title} has been removed from the library."
+        return "Book not found in the library."
 
-class Library:
-    def __init__(self):
-        self.users = {}
-        self.books = {}
-
-    def add_user(self, username, password, student_id=None):
+    def register_user(self, username, _password):
         if username not in self.users:
-            if student_id is not None:
-                user = Student(username, password, student_id)
-            else:
-                user = User(username, password)
-            self.users[username] = user
-            print(f"User '{username}' has been created.")
+            self.users[username] = _password
+            return f"User {username} has been registered."
         else:
-            print(f"User '{username}' already exists.")
+            return "Username already exists. Please choose another username."
 
-    def add_book(self, title, price):
-        self.books[title] = price
-        print(f"New book added: {title}, Price: Rs.{price:.2f}")
+    def login(self, username, _password):
+        if username in self.users and self.users[username] == _password:
+            return f"Welcome, {username}!"
+        else:
+            return "Login failed. Please check your username and password."
 
-    def list_books(self):
-        print("Books in the library:")
-        for title, price in self.books.items():
-            print(f"{title}: Rs.{price:.2f}")
+class ALL_DATA(Library, Book, ABC):  # Abstract Class
+    @abstractmethod
+    def __Data(self):  # Abstract Method
+        pass
+    print("data...................")
 
+# Create some books and add them to the library
+book1 = Book("Book1", "Author1", "ISBN111", 20.00)
+book2 = Book("Book2", "Author2", "ISBN222", 15.00)
+book3 = Book("Book3", "Author3", "ISBN333", 25.00)
 
-library = Library()
+class concreteliabrary(Library):
+    def add_book(self, book):
+        super().add_book(book) # Use super to call the base class method
+        self.books.append(book)
+        return f"{book.title} has been added to the library."
+
+my_library = concreteliabrary("My Library")
 
 while True:
-    print("Library Management System :")
-    print("1. Register a User")
-    print("2. Register a Student")
-    print("3. Log In")
-    print("4. Add a Book")
-    print("5. List Books")
-    print("6. Exit")
+    print("\nLibrary Name:", my_library.name)
+    user_input = input("Enter Your Choice:  \n Press 1 to register:  \n Press 2 for login : \n Press 3 to add a book: \n Press 4 to remove a book: \n Press q to quit: ")
 
-    choice = input("Enter your choice (1/2/3/4/5/6): ")
-
-    if choice == "1":
+    if user_input == '1':
         username = input("Enter a username: ")
         password = input("Enter a password: ")
-        library.add_user(username, password)
-    elif choice == "2":
-        username = input("Enter a username: ")
-        password = input("Enter a password: ")
-        student_id = input("Enter a student ID: ")
-        library.add_user(username, password, student_id)
-    elif choice == "3":
+        registration_result = my_library.register_user(username, password)
+        print(registration_result)
+
+    elif user_input == '2':
         username = input("Enter your username: ")
         password = input("Enter your password: ")
-        if username in library.users and library.users[username].login(password):
-            print(f"Welcome, {username}!")
-            while True:
-                print("1. Change Password")
-                print("2. List Books")
-                print("3. Log Out")
-                user_choice = input("Enter your choice (1/2/3): ")
-                if user_choice == "1":
-                    new_password = input("Enter a new password: ")
-                    library.users[username].change_password(new_password)
-                elif user_choice == "2":
-                    library.list_books()
-                elif user_choice == "3":
-                    break
+        login_result = my_library.login(username, password)
+        print(login_result)
+
+    elif user_input == '3':
+        if username in my_library.users:
+            title = input("Enter the book title: ")
+            author = input("Enter the author: ")
+            isbn = input("Enter the ISBN: ")
+            price = float(input("Enter the price: "))
+            new_book = Book(title, author, isbn, price)
+            result = my_library.add_book(new_book)
+            print(result)
         else:
-            print("Authentication failed. Please check your username and password.")
-    elif choice == "4":
-        title = input("Enter the book title: ")
-        price = float(input("Enter the book price: "))
-        library.add_book(title, price)
-    elif choice == "5":
-        print()
-        library.list_books()
-    elif choice == "6":
+            print("Login required to add a book.")
+
+    elif user_input == '4':
+        if username in my_library.users:
+            isbn = input("Enter the ISBN of the book to remove: ")
+            result = my_library.remove_book(isbn)
+            print(result)
+        else:
+            print("Login required to remove a book.")
+
+    elif user_input == 'q':
         break
-    else:
-        print("Invalid choice. Please select a valid option.")
-
-
